254bca832ea82bf51e7a682d516d117e09d8b2d4
AboodW <abed.2alwawi2000@gmail.com>
2022-03-03 00:52:48 +0300

    add new files

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+let videoUrls = ytplayer.config.args.raw_player_response.playbackTracking.map.split(",");
+console.log(".............................kfoashf",videoUrls);
+
+console.log("Our Extension has loaded Successfully");
+//ytplayer.config.args.url_encoded_fmt_stream_map //to get different option of videos urls
